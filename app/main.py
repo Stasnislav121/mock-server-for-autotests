249777f658fb732ce90f1c206ddeae7578f82225
stanislav.avdokushin <s_avdokushin@boxberry.ru>
2025-09-09 17:44:14 +0300

    фикс ручки packs

--- app/main.py
+++ app/main.py
@@ -4,17 +4,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-packs = [{
-    "id": "799",
-    "title": "Короб ХS",
-    "image": "https://storage.img.net/mobile-app-images/packs/799.png?v=1",
-    "length": 15,
-    "width": 15,
-    "height": 15,
-    "parcelExample": "мобильный телефон, украшения, духи"
-}]
+packs = {
+    "data": {
+        "boxes": [{
+            "id": "799",
+            "title": "Короб ХS",
+            "image": "https://storage.img.net/mobile-app-images/packs/799.png?v=1",
+            "length": 15,
+            "width": 15,
+            "height": 15,
+            "parcelExample": "мобильный телефон, украшения, духи"
+        }]
+    },
+    "success": True,
+    "name": "Список доступной упаковки",
+    "message": "Данные успешно загружены",
+    "time": "2023-10-25T14:30:00Z"
+}
 
 
 @app.get('/packs')
-async def get_packs() -> List[Dict[str, Union[str, int]]]:
+async def get_packs() -> dict:
     return packs
