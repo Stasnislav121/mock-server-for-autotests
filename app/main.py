9598ff6c51e1737270aacea75eb8546840317136
stanislav.avdokushin <s_avdokushin@boxberry.ru>
2025-09-09 17:10:17 +0300

    Add mock packs

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
-from fastapi import FastAPI
+from typing import List, Dict, Union
 
+from fastapi import FastAPI
 
 app = FastAPI()
 
+packs = [{
+    "id": "799",
+    "title": "Короб ХS",
+    "image": "https://storage.img.net/mobile-app-images/packs/799.png?v=1",
+    "length": 15,
+    "width": 15,
+    "height": 15,
+    "parcelExample": "мобильный телефон, украшения, духи"
+}]
+
 
 @app.get('/packs')
-async def get_packs():
-    return {
-                    "id": "799",
-                    "title": "Короб ХS",
-                    "image": "https://storage.img.net/mobile-app-images/packs/799.png?v=1",
-                    "length": 15,
-                    "width": 15,
-                    "height": 15,
-                    "parcelExample": "мобильный телефон, украшения, духи"
-        }
+async def get_packs() -> List[Dict[str, Union[str, int]]]:
+    return packs
